1a673005fbf29ccf86c9f3553fcef65259f5d13c
seboo1995 <sebair_selmani@yahoo.com>
2022-08-08 18:45:08 +0200

    added new nav item

--- src/components/MyNavBar/MyNavBar.js
+++ src/components/MyNavBar/MyNavBar.js
@@ -27,6 +27,7 @@ export const MyNavBar = () => {
                 <div className="menu" onClick={close_ham}>
                     <ul>
                         <li><a href="#">00 Home </a></li>
+                        <li><a href="#">05 Added </a></li>
                         <li><a href="#">01 Destination</a></li>
                         <li><a href="#">02 Crew </a></li>
                         <li><a href="#">03 Technology </a></li>
