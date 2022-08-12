d9afe893f74c7ea7bd8843c20629d58cb2260eca
Darko <darko.stojanovski64@gmail.com>
2022-08-12 12:16:27 +0200

    added data tech,crew and css

--- src/components/crew/Crew.jsx
+++ src/components/crew/Crew.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
+import './Crew.css'
+import myData from "../../assets/data.json"
+import { useState } from 'react'
 
 const Crew = () => {
+  const all_crew = myData.crew
+  const [imageSrc, setImageSrc] = useState(all_crew[0].images.png)
+  const [role, setRole] = useState(all_crew[0].role)
+  const [bio, setBio] = useState(all_crew[0].bio)
+  const [crewname, setCrewName] = useState(all_crew[0].name)
+
+  const changeSrc = (e) => {
+    const t = all_crew.filter((obj) => {
+    if (e.currentTarget.textContent.toLowerCase().trim() ===  obj.name.toLowerCase().trim() ) {
+        return true
+    }})
+    setRole(t[0].role)
+    setBio(t[0].bio)
+    setCrewName(t[0].name)
+    setImageSrc(t[0].images.png)
+    all_crew.forEach((obj) => {
+      if (e.currentTarget.textContent.toLowerCase().trim() === obj.name.toLowerCase().trim()) {
+          document.getElementById(obj.name).style.opacity = 0.5
+      }
+      else {
+        document.getElementById(obj.name).style.opacity = 1
+      }
+
+
+    })
+   
+  }
+
   return (
-    <div>Crew</div>
+    <div className='crew-container'>
+
+    <h2>Meet your crew </h2>
+     <img src={imageSrc} alt = 'asl' id='222'/> 
+
+      <ul>
+        {all_crew.map((crew) => {
+          return (
+            <button onClick={ changeSrc} key={crew.name} id={crew.name}> {crew.name}</button>
+
+            
+          )
+        })}  
+    </ul>
+    <p> {bio}</p>
+    <h3> {role}</h3>
+    <br></br>
+    <h3>{crewname}</h3>
+    </div>
   )
 }
 
--- src/components/technology/Technology.jsx
+++ src/components/technology/Technology.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import {React,useState} from 'react'
+import './Technology.css'
+import myData from '../../assets/data.json';
 
 const Technology = () => {
+  const all_technology = myData.technology
+  const [imageSrc, setImageSrc] = useState(all_technology[0].images.portrait)
+  const [techname, setTechName] = useState(all_technology[0].name)
+  const [desc, setDesc] = useState(all_technology[0].description)
+
+  
+  const changeSrc = (e) => {
+    const t = all_technology.filter((obj) => {
+    if (e.currentTarget.textContent.toLowerCase().trim() ===  obj.name.toLowerCase().trim() ) {
+        return true
+    }})
+    setDesc(t[0].description)
+    setTechName(t[0].name)
+    setImageSrc(t[0].images.portrait)
+    all_technology.forEach((obj) => {
+      if (e.currentTarget.textContent.toLowerCase().trim() === obj.name.toLowerCase().trim()) {
+          document.getElementById(obj.name).style.opacity = 0.5
+      }
+      else {
+        document.getElementById(obj.name).style.opacity = 1
+      }
+
+
+    })
+   
+  }
+
   return (
-    <div>Technology</div>
+       <div className='technology-container'>
+
+    <h2> SPACE LAUNCH 101 </h2>
+     <img src={imageSrc} alt = 'asl' id='222'/> 
+
+      <ul>
+        {all_technology.map((technology) => {
+          return (
+            <button onClick={ changeSrc} key={technology.name} id={technology.name}> {technology.name}</button>
+
+            
+          )
+        })}  
+    </ul>
+    <p> {desc}</p>
+    <br></br>
+    <h3> {techname}</h3>
+    </div>
   )
 }
 
